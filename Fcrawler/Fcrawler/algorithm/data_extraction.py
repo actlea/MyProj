c8c36baa67b2e5a6f67a95413992baf034f099da
actlea <zjm1169919024@163.com>
2015-06-26 14:17:54 +0800

    $1

--- Fcrawler/Fcrawler/algorithm/data_extraction.py
+++ Fcrawler/Fcrawler/algorithm/data_extraction.py
@@ -19,8 +19,7 @@ TEXT_FINDER_XPATH2 = '//body\
                             self::style or \
                             self::i or \
                             self::b or \
-                            self::strong or \
-                            self::span \
+                            self::strong \
                             )] \
                             /text()/..'
 
@@ -30,13 +29,19 @@ def parse(html_str, encoding='utf-8'):
     #only textnode and linknode reserved
     subtrees = get_textnode_subtrees(html_tree, xpath_to_text=TEXT_FINDER_XPATH2)    
     
-    #conuter paths
+    #first gather paths
     paths = gather_xpath(subtrees) 
        
     #remove pre-path
     pre_removed_path = remove_prepath(paths)
+    target_subtrees = [subtree for subtree in subtrees
+                       if is_path_in(subtree.parent_path, pre_removed_path)]
     
-    for path in pre_removed_path:
+    #second gather path
+    second_gather_paths = second_gather_xpath(pre_removed_path)
+    
+    
+    for path in second_gather_paths:
         print path
 
 '''
@@ -52,18 +57,43 @@ def remove_prepath(paths):
         if trie_obj.isend(path[0]):
             tmp.append(path)
     return tmp
-    
 
-def pre_match(path1, path2):
-    t1 = path1.split('/')
-    t2 = path2.split('/')
-    if len(t1)>=len(t2):
-        return False
-    for i in range(len(t1)):
-        if t1[i] != t2[i]:
-            return False
-    return True
     
+def second_gather_xpath(paths):
+    tmp = []
+    splitpaths = [p[0].rsplit('/', 1) for p in paths] 
+    parentpaths = [p[0] for p in splitpaths]
+    # build frequency distribution
+    parentpaths_counter = Counter(parentpaths)
+    pre_paths = parentpaths_counter.most_common()   
+    
+    trie_obj = LBTrie()
+    for path in paths:
+        trie_obj.add(path[0])
+        
+    seen={}
+       
+    for pre in pre_paths:
+        if pre[1]==1: break
+        if not trie_obj.isend(pre[0]):
+            tmp.append(pre) 
+    
+    for path in paths:
+        flag = True
+        for p in tmp:
+            if pre_match(p[0], path[0]):
+                flag=False
+                break
+        if flag:
+            tmp.append(path)            
+                   
+    return tmp    
+        
+    
+    
+        
+    
+ 
 
 def gather_xpath(subtrees):
     #all xpaths
@@ -78,6 +108,28 @@ def gather_xpath(subtrees):
     parentpaths_counter = Counter(parentpaths)
     return parentpaths_counter.most_common()
 
+
+
+'''test if parent_path in paths'''    
+def is_path_in(parent_path, paths):                
+    trie_obj = LBTrie()
+    for path in paths:
+        trie_obj.add(path[0])
+    if trie_obj.search(parent_path):
+        return True
+    return False    
+
+'''test if path1 if prefix of path2'''        
+def pre_match(path1, path2):
+    t1 = path1.split('/')
+    t2 = path2.split('/')
+    if len(t1)>=len(t2):
+        return False
+    for i in range(len(t1)):
+        if t1[i] != t2[i]:
+            return False
+    return True
+    
 '''
 use Trie Tree to remove pre path
 '''
@@ -110,6 +162,19 @@ class LBTrie:
             return False
         return True
     
+    def search(self, word):  
+        p = self.trie 
+        for c in word:  
+            if not c in p:  
+                return False  
+            p = p[c]  
+        #判断单词结束标记''  
+        if '' in p:  
+            return True  
+        return False
+    
+        
+          
 
 
 if __name__=='__main__':
